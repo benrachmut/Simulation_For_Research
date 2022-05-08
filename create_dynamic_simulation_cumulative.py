932046816631373a9210c711493f92ff836d3c6c
benrachmut <rachmut.ben@gmail.com>
2022-05-08 15:40:25 +0300

    fdgfdg

--- create_dynamic_simulation_cumulative.py
+++ create_dynamic_simulation_cumulative.py
@@ -56,7 +56,7 @@ def get_group_by_scenario_by_importance(organized_data, data_scenario_by_index, 
 
 def get_data_time_simNumber_indexs(data_group_by_scenario_via_list,max_time,fisher_data_jumps):
     ans = {}
-    for time_ in range(0, max_time *2 , fisher_data_jumps*5 ):
+    for time_ in range(0, int(max_time *1.5), fisher_data_jumps*2):
         ans[time_] = {}
         for sim_number, dict_measure_listData in data_group_by_scenario_via_list.items():
             ans[time_][sim_number] = []
@@ -82,17 +82,30 @@ def get_data_prior_cumulative(data_time_simNumber_indexs, data_group_by_scenario
                     ans[time_][sim_number][key] = []
                     for i in index_list:
                         ans[time_][sim_number][key].append(data_group_by_scenario_via_list[sim_number][key][i])
-
-
-
     return ans
 
 
+def get_data_cumulative_dict(data_prior_cumulative):
+    ans= {}
+    for time_, scenario_number_dict in data_prior_cumulative.items():
+        ans[time_] = {}
+        for scenario_number,measure_data_dict in scenario_number_dict.items():
+            for measure_,data_list in measure_data_dict.items():
+                if measure_ not in ans[time_]:
+                    ans[time_][measure_] = []
+                if len(data_list) == 0:
+                    ans[time_][measure_].append(0)
+                else:
+                    ans[time_][measure_].append(sum(data_list))
+    return ans
+
+
+def calculate_avg_data_cumulative(data_cumulative_dict):
+    pass
+
+
 def make_dynamic_simulation_cumulative(organized_data ,fisher_data_jumps,name_,keys =
-        ["Total Time In System",
-        "Arrival Delay",
-        "Cap",
-        "Abandonment Penalty"]):
+        ["Total Time In System","Arrival Delay","Cap","Abandonment Penalty"]):
 
     max_time = int(get_max_time_in_system(organized_data))+1
 
@@ -104,6 +117,6 @@ def make_dynamic_simulation_cumulative(organized_data ,fisher_data_jumps,name_,k
 
     data_time_simNumber_indexs = get_data_time_simNumber_indexs(data_group_by_scenario_via_list,max_time,fisher_data_jumps)
     data_prior_cumulative = get_data_prior_cumulative(data_time_simNumber_indexs, data_group_by_scenario_via_list,keys)
-    data_cumulative_dict = get_data_cumulative_dict
-
+    data_cumulative_dict = get_data_cumulative_dict(data_prior_cumulative)
+    data_cumulative_dict_for_panda = calculate_avg_data_cumulative(data_cumulative_dict)
 
